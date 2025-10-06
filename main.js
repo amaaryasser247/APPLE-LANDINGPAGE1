79f2d35c9a740997992bc69d5656068180e469ef
amaaryasser247 <amaar202202769@gmail.com>
2025-10-06 17:15:54 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+
+function phones(phoneSrc) {
+  img.src = phoneSrc;
+}
+
+function changecolor(color) {
+  container.style.background = color;
+}
